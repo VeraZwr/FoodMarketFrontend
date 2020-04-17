bf519bf5018ac725ed4da27eb543a6af99bec7ab
VeraZwr <vera.zwr@hotmail.com>
2020-04-16 20:28:33 -0400

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DefaultComponent} from './default/default.component';
+import {LoginComponent} from './user/login/login.component';
+import {SignupComponent} from './user/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'default',
+    component: DefaultComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'signup', component: SignupComponent}
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DefaultModule} from './default/default.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {SharedModule} from './shared/shared.module';
+import {CategoryModule} from './category/category.module';
+import {ProductsModule} from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
+    CategoryModule,
+    ProductsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
